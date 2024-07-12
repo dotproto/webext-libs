0dc7c0ec5d568df5aa448bc640137f877c42249b
Simeon Vincent <svincent@gmail.com>
2024-07-12 12:32:22 -0700

    Rewrite bond() docs

--- shared/bind.js
+++ shared/bind.js
@@ -1,10 +1,37 @@
-// Assign a bound version of a property on the prototype chain to the instance
-// itself. This is useful in situations where a callback handler is defined on
-// an object's prototype but the handler must be bound to the instance in order
-// to preserver `this` references.
-//
-// The main disadvantage to this approach is that the bound version of the
-// function does not reflect changes to the underlyig
+// This file contains a couple of different alternatives methods of binding
+// functions to a particular context. While basic `fn.bind()` calls are
+// extremely useful, in some situations you may want a more expressive way to
+// perform that binding or need subtly different behavior from the typical
+// pattern.
+
+/**
+ * Binds a method on an object's prototype chain to the object itself and return
+ * a reference to the bound method. `bond()` is similar to
+ * `Function.prototype.bind()`, but has a few key differences:
+ *
+ * 1. In addition to returning a bound version of the method, this function will
+ *    also assign the bound method to the `instance`.
+ * 2. `bond()` _only_ binds methods declared on the object's prototype chain. If
+ *    the object already has a member with that name, it will be replaced.
+ * 3. If a method cannot be found on the object's prototype chain, the `bond()`
+ *    call will throw. This behavior is similar to how a `.bind()` call on a
+ *    non-existent function will throw.
+ * 4. `bond()` provides a shorter, more expressive syntax for performing the
+ *    conventional binding of a class's method on in instance.
+ *
+ * @example
+ * class Example {
+ *   constructor(eventTarget) {
+ *     eventTarget.addEventListener("message", bond(this, "handleMessage"));
+ *   }
+ *
+ *   handleMessage() { ... }
+ * }
+ * @param {*} instance - The object that will be searched for the method and to
+ * which the bound method will be assigned.
+ * @param {*} methodName The name of the method `instance`'s prototype chain.
+ * @returns The bound method
+ */
 export function bond(instance, methodName) {
   instance[methodName] = Reflect.getPrototypeOf(instance)[methodName].bind(instance);
 }
